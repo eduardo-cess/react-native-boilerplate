26fd2751a9a060fe4e24fd3a23fffc5a1d4443cc
carlos <eeduardosantiago@gmail.com>
2018-04-30 20:53:50 -0300

    refatorando com async await

--- src/store/actions/produtos.js
+++ src/store/actions/produtos.js
@@ -1,23 +1,14 @@
 
-// import { getAllProdutosFirebase } from "../functions/produto";
+import { getAllProdutosFirebase } from "../functions/produto";
 import { GET_ALL_PRODUTOS } from "./actionTypes";
 import { db } from "../../config/firebase";
 
-var ProdutosRef = db.collection("produtos");
-
 export const getAllProdutos = () => {
-  return dispatch => {
-    ProdutosRef.get()
-      .then(function(querySnapshot) {
-        let allProdutos = []
-        querySnapshot.forEach(function(doc) {
-          // console.log(doc.id, " => ", doc.data());
-          allProdutos.push(doc.data())
-        });
-        dispatch({
-          type: GET_ALL_PRODUTOS,
-          payload: allProdutos
-        })
-      });
+  return async dispatch => {
+    let allProdutos = await getAllProdutosFirebase()
+    dispatch({
+      type: GET_ALL_PRODUTOS,
+      payload: allProdutos
+    })
   };
 };
--- src/store/functions/produto.js
+++ src/store/functions/produto.js
@@ -3,26 +3,17 @@ import {db} from "../../config/firebase"
 var produtosRef = db.collection("produtos");
 
 export const getAllProdutosFirebase = async () => {
-    let produtos = []
     let produtosQuery = await produtosRef.get()
-        // .then(function(querySnapshot) {
-        //     querySnapshot.forEach(function(doc) {
-        //     allProdutos = this.firebaseDataToArray(querySnapshot)
-        //     console.log(doc.id, " => ", doc.data());
-            
-        //     });
-        // });
-    produtos = firebaseDataToArray(produtosQuery)
+    let produtos = firebaseDataToArray(produtosQuery)
     return produtos
 }
 
-var firebaseDataToArray = function (querySnapshot) {
+export const getProduto = (idProduto) => {}
+
+const firebaseDataToArray = function (querySnapshot) {
     let data = []
     querySnapshot.forEach(doc => {
-      data.push(doc.data())
+        data.push(doc.data())
     })
     return data
-  }
-  
-
-export const getProduto = (idProduto) => {}
+}

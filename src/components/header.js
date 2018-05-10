485b19332e3137ab2976c2bfc569fe13c596a7a9
carlos <eeduardosantiago@gmail.com>
2018-05-10 20:43:12 -0300

    primeiro layout da barra de pesquisa

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,74 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
-import { Header, Body, Left, Button, Icon, Right, Title, Drawer } from "native-base";
+import {
+  Header,
+  Body,
+  Left,
+  Button,
+  Icon,
+  Right,
+  Title,
+  Drawer,
+  Item,
+  Input
+} from "native-base";
 
 class AppHeader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searching: false
+    };
+  }
+
+  search = () => {
+    this.setState((prevState, props) => {
+      return {searching: !prevState.searching};
+    });
+  }
+
   render() {
-    return (
+    let mainHeader = (
       <Header noShadow hasTabs>
         <Left>
           <Button transparent onPress={() => this.props.leftButtonPress()}>
-            <Icon name={(this.props.title === 'Bem Vindo') ? "menu" : "md-arrow-back"} />
+            <Icon
+              name={this.props.title === "Bem Vindo" ? "menu" : "md-arrow-back"}
+            />
           </Button>
         </Left>
         <Body>
           <Title>{this.props.title}</Title>
         </Body>
         <Right>
-          <Button transparent>
+          <Button transparent onPress={() => this.search()}>
             <Icon name="search" />
           </Button>
         </Right>
       </Header>
     );
+
+    let searchHeader = (
+      <Header searchBar rounded>
+         <Left>
+            <Button transparent onPress={() => this.search()}>
+              <Icon name="md-arrow-back" />
+            </Button>
+         </Left>
+        <Right style={{paddingTop: 15, paddingBottom: 15,}}>
+          <Item rounded style={{backgroundColor: "white"}}>
+            <Input placeholder="Pesquisar" />
+          </Item>
+          <Button transparent onPress={() => this.search()}>
+            <Icon name="search" />
+          </Button>
+        </Right>
+      </Header>
+    );
+
+    let header = this.state.searching === false ? mainHeader : searchHeader;
+    
+    return <View>{header}</View>;
   }
 }
 
